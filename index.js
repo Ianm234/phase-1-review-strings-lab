3dbf386fe5418987be4b04a5d6f0b54b0ad04794
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-03-14 17:07:28 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const 'currentUser'='The Creed';
+const 'welcomeMessage'='Welcome to Flatbook, $(currentUser)!';
+'excitedwelcomemessage'='welcomeMessage.toUpperCase();
+const 'shortgreeting'='currentUser.slice(0, 1);
+const 'shortgreeting'='Welcome, $[currentUser.slice(0, 1)]!';
